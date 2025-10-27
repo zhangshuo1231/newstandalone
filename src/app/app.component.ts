8e91b97852240658f771a033017db8acbd4ae60a
zhang <zhangshuo1231@gmail.com>
2025-10-26 20:12:37 -0400

    Commit after the first learning day.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, HomeComponent, CommonModule, ReversePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'newstandalone';
+  title = 'Angular 18 Tutorial';
+  subtitle = 'Angular for Beginners';
+  todayDate = new Date();
+  salary = 200000.00;
+  _object = { name: 'Angular', version: 18 };
+
+  isdisabled = false;
 }
